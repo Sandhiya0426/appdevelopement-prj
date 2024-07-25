23125ee0ab00b81890e458877784c3874a2d0d90
sandhiya.m <sandhiy.m0426@gmail.com>
2024-07-25 14:09:05 +0530

    auto

--- frontend/src/components/AutoCarousel.js
+++ frontend/src/components/AutoCarousel.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import './styles/AutoCarousel.css'; // Make sure this path is correct
 
-import './styles/AutoCarousel.css'
+// Import images
+import imgwed2 from '../assets/imgwed2.jpeg'; // Ensure this path is correct
+import imgwed3 from '../assets/imgwed3.jpg'; // Ensure this path is correct
 
-import imgwed2 from '../assets/imgwed2.jpeg';
-import imgwed3 from '../assets/imgwed3.jpg';
-
-// import hd from '../assets/Images/hd.jpg';
-// import i5 from '../assets/Images/i5.jpg';
-// import new1 from '../assets/Images/new1.jpg';
-const images = [
-  imgwed2, imgwed3
-];
+const images = [imgwed2, imgwed3];
 
 const AutoCarousel = () => {
   return (
     <div className='car'>
-    <Carousel interval={3000} /* Set the interval in milliseconds */>
-      {images.map((image, index) => (
-        <Carousel.Item key={index}>
-          <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
-        </Carousel.Item>
-      ))}
-    </Carousel>
-       {/* <br /><h2 style={{color:'white',textAlign:'center'}}>CATOGERIES</h2><br />
-    <Cardofcat/> */}
+      <Carousel interval={3000}> {/* Set the interval in milliseconds */}
+        {images.map((image, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100" src={image} alt={`Slide ${index + 1}`} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 };
